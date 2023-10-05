5b42cf1d71b054fbb6fb7ba14d37ccf28e6d21d2
anoyash <ak2417942@gmail.com>
2023-10-05 12:43:21 +0530

    updated Cubes component

--- src/Componets/Cubes.jsx
+++ src/Componets/Cubes.jsx
@@ -2,8 +2,7 @@ import Spline from '@splinetool/react-spline';
 
 export default function App() {
   return (
-    <>
     <Spline scene="https://prod.spline.design/LPx0qzf-E2GHFajP/scene.splinecode" />
-    </>
   );
 }
+
